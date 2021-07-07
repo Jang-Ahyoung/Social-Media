3e581294d7da75e3ea1211b29bdad1b09aa86fc0
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-07-07 21:32:45 +0900

    ✅ 작성 보완 및 사진 업로드 기능

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,21 +1,54 @@
 import styles from './Share.module.css';
 import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
+import { useContext, useRef, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
 
 export default function Share() {
+    const { user } = useContext(AuthContext);
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const post = useRef();
+    const [file, setFile] = useState(null);
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const newPost = {
+            userId: user._id,
+            desc: post.current.value,
+        };
+        if (file) {
+            const data = new FormData();
+            const fileName = Date.now() + file.name;
+            data.append("name", fileName);
+            data.append("file", file);
+            newPost.img = fileName;
+            try {
+                await axios.post("/upload", data);
+            } catch (err) { }
+        }
+        try {
+            await axios.post("/posts", newPost);
+            window.location.reload();
+        } catch (err) { }
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.Top}>
                 <div className={styles.User}>
-                    <img className={styles.UserImg} src="/assets/person/1.jpeg" alt="userProflie" />
-                    <span className={styles.UserName}> Ahyoung</span>
+                    <img className={styles.UserImg} src={user.profilePicture ? PF + user.profilePicture : PF + "noAvatar.png"} alt="userProflie" />
+                    <span className={styles.UserName}>{user.username}</span>
                 </div>
-                <input className={styles.Input} placeholder="What's in your mind Ahyoung?" type="text" />
+                <input className={styles.Input} ref={post} placeholder={`What's in your mind ${user.username}?`} type="text" />
             </div>
             <hr className={styles.hr} />
-            <div className={styles.Bottom}>
+            <form className={styles.Bottom} onSubmit={submitHandler}>
                 <div className={styles.options}>
-                    <PermMedia htmlColor="tomato" className={styles.optionsIcon} />
-                    <span className={styles.optionsText}>Photo / Video</span>
+                    <label htmlFor="file" className={styles.fileInput}>
+                        <PermMedia htmlColor="tomato" className={styles.optionsIcon} />
+                        <span className={styles.optionsText}>Photo / Video</span>
+                        <input style={{ display: "none" }} type="file" id="file" accept=".png,.jepg,.jpg"
+                            onChange={(e) => setFile(e.target.files[0])} />
+                    </label>
                     <Label htmlColor="blue" className={styles.optionsIcon} />
                     <span className={styles.optionsText}>Tag</span>
                     <Room htmlColor="green" className={styles.optionsIcon} />
@@ -23,8 +56,8 @@ export default function Share() {
                     <EmojiEmotions htmlColor="golenrod" className={styles.optionsIcon} />
                     <span className={styles.optionsText}>Feelings</span>
                 </div>
-                <button className={styles.Button}><span>Share</span></button>
-            </div>
+                <button className={styles.Button} type="submit"><span>Share</span></button>
+            </form>
         </div>
     )
 }
