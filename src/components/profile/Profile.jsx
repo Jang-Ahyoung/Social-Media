276a0ed61fe7ba28aab44c1c91c95422eaa4a0f4
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-07-08 14:38:03 +0900

    🤝팔로잉 / 언팔 버튼 추가 및 api 연결

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import { useParams } from 'react-router';
 import styles from './Profile.module.css';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Feed from '../feed/Feed';
 import Rightbar from '../rightbar/Rightbar';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Profile() {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const { user: loginUser, dispatch } = useContext(AuthContext);
     const [user, setUser] = useState({});
+    const [follow, setFollow] = useState(loginUser.followings ? loginUser.followings.includes(user._id) : false);
     const parmas = useParams();
     useEffect(() => {
         const fetchUser = async () => {
@@ -16,6 +19,25 @@ export default function Profile() {
         }
         fetchUser();
     }, [parmas.username]);
+
+    useEffect(() => {
+        setFollow(loginUser.followings.includes(user?._id));
+    }, [loginUser, user?._id])
+
+    const followHandler = async () => {
+        try {
+            if (!follow) {
+                await axios.put(`/users/${user._id}/follow`, { userId: loginUser._id });
+                dispatch({ type: "FOLLOW", payload: user._id });
+            } else {
+                await axios.put(`/users/${user._id}/unfollow`, { userId: loginUser._id });
+                dispatch({ type: "UNFOLLOW", payload: user._id });
+            }
+        } catch (err) {
+            console.log(err);
+        }
+        setFollow(!follow);
+    }
 
     return (
         <div className={styles.container}>
@@ -27,6 +49,7 @@ export default function Profile() {
                 <div className={styles.ProfileInfo}>
                     <h4 className={styles.name}>{user.username}</h4>
                     <span className={styles.desc}>{user.desc}</span>
+                    {user._id !== loginUser._id && <button className={follow ? styles.unfollowBtn : styles.followBtn} onClick={followHandler}> {follow ? "- Unfollow" : "+ Fllow"} </button>}
                 </div>
             </div>
             <div className={styles.Bottom}>
