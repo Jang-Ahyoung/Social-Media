2001499a5a71979e83dbf34925664092dbad17f4
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-07-08 14:35:37 +0900

    👋 필드 정렬 및 로그인 Context 연결

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -3,21 +3,25 @@ import Post from '../post/Post';
 import Share from '../share/Share';
 import styles from './Feed.module.css';
 import axios from 'axios';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Feed({ username }) {
     const [posts, setPosts] = useState([]);
+    const { user } = useContext(AuthContext);
     useEffect(() => {
         const fetchPosts = async () => {
-            const res = username ? await axios.get("/posts/profile/" + username) : await axios.get("posts/timeline/60e0469b2932308600a985ef");
-            setPosts(res.data);
+            const res = username ? await axios.get("/posts/profile/" + username) : await axios.get("posts/timeline/" + user._id);
+            setPosts(res.data.sort((post1, post2) => {
+                return new Date(post2.createdAt) - new Date(post1.createdAt);
+            }));
         }
         fetchPosts();
-    }, [])
-
+    }, [username, user._id]);
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-                <Share />
+                {(!username || username == user.username) && <Share />}
                 {posts.map((post) => (
                     <Post key={post._id} post={post} />
                 ))}
