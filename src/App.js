5c088de0129a17c9624d0f2f75f02a783bd6a91d
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-07-10 16:59:05 +0900

    🛠 페이지 router 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import UserProfile from "./pages/userprofile/UserProfile";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
 function App() {
-  return <div>hello</div>;
+  const { user } = useContext(AuthContext);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home user /> : <Register />}
+        </Route>
+        <Route exact path="/login">
+          {user ? <Redirect to="/" /> : <Login />}
+        </Route>
+        <Route exact path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/profile/:username" component={UserProfile} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
